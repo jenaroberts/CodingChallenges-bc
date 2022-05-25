741091a7cb34e17ae7d91def32fc3b8b99d8f358
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-05-25 13:07:48 -0400

    my-first-commit

---
+++ coding-challenges.js
@@ -0,0 +1,17 @@
+const nameArr = ["Jena"];
+function reverseArr(arr) {
+  return arr.reverse().toString();
+}
+console.log(reverseArr(nameArr));
+
+// return String.split().reverse().join();
+
+const jewels = "z";
+const stones = "ZZ";
+
+let sum = 0;
+if (stones === jewels) {
+  console.log(c + 1);
+} else {
+  console.log(sum);
+}
---
+++ weekSixChallenges.js
@@ -0,0 +1,12 @@
+// // given an array see if it is a palindromes and return true or false.
+
+// //using an array or string method
+const pArray = ["moerm"];
+const arrReversed = pArray.reverse();
+if (pArray !== arrReversed) {
+  console.log(false);
+} else {
+  console.log(true);
+}
+
+//using a loop function
